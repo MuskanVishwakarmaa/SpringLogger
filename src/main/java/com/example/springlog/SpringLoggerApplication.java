54f05398c19114e4ca92c414e44c8b2a7e8b9bd6
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-03-02 04:01:04 +0530

    UC2

--- src/main/java/com/example/springlog/SpringLoggerApplication.java
+++ src/main/java/com/example/springlog/SpringLoggerApplication.java
@@ -1,18 +1,24 @@
 package com.example.springlog;
 
 import com.example.springlog.component.DemoBean;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
 
 @SpringBootApplication
 
 public class SpringLoggerApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringLoggerApplication.class);
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concept");
+		logger.debug("Welcome to spring concept");
 		ApplicationContext context = SpringApplication.run(SpringLoggerApplication.class, args);
-		System.out.println("Checking Context: " + context.getBean(DemoBean.class));
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+		logger.debug("\n*** Example using @Autowire annotation on property ***");
 	}
 
 }
